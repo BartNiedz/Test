860fe80f20a8da196564a33259068fb79a95e4c7
BartNiedz <natovskyy@gmail.com>
2024-05-22 16:10:19 +0200

    Trojkat
    -dwie metody: PP i Obw
    -kontroler i akcja
    -w akcji u podaje 3 parametry

---
+++ Test/Test/Models/Trojkat.cs
@@ -0,0 +1,23 @@
+﻿namespace Test.Models
+{
+    public class Trojkat
+    {
+        public int a {  get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+        /*public Trojkat(int _a, int _b, int _c)
+        {
+            a = _a; 
+            b = _b; 
+            c = _c;
+        }*/
+        public int PP()
+        {
+            return (a * b) / 2;
+        }
+        public int Obw()
+        {
+            return a + b + c;
+        }
+    }
+}
